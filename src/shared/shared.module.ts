425e666706576e8f8fcfc7a4feaf6cc779bd7ce2
Victor Raul Herrera Castillo <u20201e188@upc.edu.pe>
2023-06-13 21:10:55 -0500

    chore: configured database provider.

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './infrestructure/database/database.module';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule]
+})
 export class SharedModule {}
